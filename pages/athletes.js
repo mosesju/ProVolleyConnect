4e660c2526104915e8ee4fcfcf134313c406a155
mosesju <edwinjmoses@lewisu.edu>
2021-10-31 22:34:44 +0100

    basic frame

---
+++ pages/athletes.js
@@ -0,0 +1,7 @@
+export default function Athletes() {
+    return (
+        <div>
+            Athletes
+        </div>
+    )
+}
---
+++ pages/parents.js
@@ -0,0 +1,7 @@
+export default function Parents() {
+    return (
+        <div>
+            Parents
+        </div>
+    )
+}
