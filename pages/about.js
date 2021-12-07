81b61a85e0ad066e1c249bbb6be144a8aa630af8
mosesju <edwinjmoses@lewisu.edu>
2021-12-07 14:32:32 +0100

    added to about us

--- pages/about.js
+++ pages/about.js
@@ -12,33 +12,54 @@ export default function Home() {
         headline: 'About Us',
         buttonText: 'Get your FREE membership!',
         buttonLink: 'https://blog.volleyhead.com/#/portal/signup',
-        subHeading: 'We are a neutral group of former pros and coaches who want to make stories and information about pro Volleyball available to every Volleyball fan.'
+        subHeading: 'We are a neutral group of former pros and coaches who are making professional Volleyball more accessible. We interview the top athletes to hear their stories and learn what being a pro is actually like'
     }
     const headProps = {
         title: 'About Volleyhead',
         metaContent: ''
     }
+    const julianPicture = "https://xvunwquprjgywjthgato.supabase.in/storage/v1/object/sign/util-photos/headshot.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJ1dGlsLXBob3Rvcy9oZWFkc2hvdC5qcGciLCJpYXQiOjE2Mzg4ODMyOTksImV4cCI6MTk1NDI0MzI5OX0.RL8i_PD90zBVTQl6Xc6PS71w7s8o95ItmeZrTV5SY6E"
+    const scottPicture = "https://xvunwquprjgywjthgato.supabase.in/storage/v1/object/sign/util-photos/Scott-headshot?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJ1dGlsLXBob3Rvcy9TY290dC1oZWFkc2hvdCIsImlhdCI6MTYzODg4MzI2MywiZXhwIjoxOTU0MjQzMjYzfQ.K9sDd9gJyXoEIrPwK92bJ16tDiZ5TqhPZc3_wtBY0pw"
+
     return (
             <main className={styles.main}>
                 <Head props={ headProps } />
                 <NavBar />
-                <Jumbotron props={ jumboProps } />
                 <div className="col-md-8">
-                    <p className={styles.description}>What is Volleyhead?</p>
+                <Jumbotron props={ jumboProps } />
+                    {/* <p className={styles.description}>What is Volleyhead?</p> */}
                     <h2 className={styles.subTitle}>Our Team</h2>
                     <div className="card">
                         <div className="card-body"> 
                         <div className="d-flex flex-row bd-highlight mb-3">
                             <img 
-                                src={johnSperaw} 
+                                src={julianPicture} 
                                 className="d-flex justify-content-center rounded-circle image-fluid"
                                 height="150" width="150"
                             />
                             <div className="p-2 bd-highlight mb-auto">
                             <div className="d-flex flex-column bd-highlight mb-3">
-                                <p className="display-6">John Speraw</p>
-                                <h6> Head College &amp; USA Men&apos;s National Team Coach</h6>
-                                <div className="lead">I&apos;m excited to have a trusted resource for professional volleyball information. Over the last few years I&apos;ve had athletes consistently come up to me and ask what it takes to go pro.  How do they find an agent? When does the process start? What are the NCAA rules? The list goes on and on. Finally I can point them to a resource that has all the information they need and the stories of the athletes who have made the process work. </div>
+                                <p className="display-6">Julian Moses</p>
+                                <h6>Co-Founder and Content Writer</h6>
+                                {/* <div className="lead">I&apos;m excited to have a trusted resource for professional volleyball information. Over the last few years I&apos;ve had athletes consistently come up to me and ask what it takes to go pro.  How do they find an agent? When does the process start? What are the NCAA rules? The list goes on and on. Finally I can point them to a resource that has all the information they need and the stories of the athletes who have made the process work. </div> */}
+                            </div>
+                            </div>
+                        </div>            
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body"> 
+                        <div className="d-flex flex-row bd-highlight mb-3">
+                            <img 
+                                src={scottPicture} 
+                                className="d-flex justify-content-center rounded-circle image-fluid"
+                                height="150" width="150"
+                            />
+                            <div className="p-2 bd-highlight mb-auto">
+                            <div className="d-flex flex-column bd-highlight mb-3">
+                                <p className="display-6">Scott Kevorken</p>
+                                <h6>Co-Founder and Content Writer. Volunteer assistant: Pepperdine</h6>
+                                {/* <div className="lead">I&apos;m excited to have a trusted resource for professional volleyball information. Over the last few years I&apos;ve had athletes consistently come up to me and ask what it takes to go pro.  How do they find an agent? When does the process start? What are the NCAA rules? The list goes on and on. Finally I can point them to a resource that has all the information they need and the stories of the athletes who have made the process work. </div> */}
                             </div>
                             </div>
                         </div>            
@@ -52,7 +73,7 @@ export default function Home() {
                     <p className={styles.description}>
                         If you want to have access to our Discord community, weekly email, more stories and exclusive content
                         you&apos;ll want to opt into our paid membership. It&apos;s $99 per year. If you&apos;re a junior or college player
-                        who can&apos;t afford our paid plan, send us an email and we can figure something out.
+                        , send us an email and we can figure something out.
                     </p>
                 </div>
                 <Footer />
