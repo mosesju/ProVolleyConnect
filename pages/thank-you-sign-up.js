cf0718b9c40b85dd5c0af309456f55b162a5687b
mosesju <edwinjmoses@lewisu.edu>
2022-01-27 15:59:49 +0100

    giphy error fixed

--- pages/thank-you-sign-up.js
+++ pages/thank-you-sign-up.js
@@ -11,7 +11,7 @@ export default function ThankYou() {
                 <div className="col-md-8">
                     <h1 className={styles.title}>Thanks for joining Volleyhead</h1>
                     <h2>This is how excited we are!</h2>
-                    <iframe src="https://giphy.com/embed/P8YWjInpYxwIC4bTjt" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/volleyballworld-serbia-srbija-nikola-pekovic-P8YWjInpYxwIC4bTjt"></a></p>                </div>
+                    <iframe src="https://giphy.com/embed/P8YWjInpYxwIC4bTjt" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/volleyballworld-serbia-srbija-nikola-pekovic-P8YWjInpYxwIC4bTjt"></a></p>                </div>
                 <Footer />
             </main>
         
