609b05135743c40f3fb2fe1de7db1a38e3b612d5
mosesju <edwinjmoses@lewisu.edu>
2022-01-30 19:53:23 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import Script from "next/script"
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return(
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-Q3KS86QZ8Q`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Q3KS86QZ8Q', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+    
+  )
 }
 
 export default MyApp
