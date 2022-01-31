f57d2f1a24e55b4fe4e1d1fef74abaceab71974e
mosesju <edwinjmoses@lewisu.edu>
2022-01-31 22:14:01 +0100

    removed signup button convert kit

--- pages/components/ConvertKitSignUp.js
+++ pages/components/ConvertKitSignUp.js
@@ -6,9 +6,9 @@ export default function ConvertKitSignUp() {
     // Is this going to be loaded from supabase
     return (
         <div>
-            <Link href="https://dogged-pioneer-9796.ck.page/ad5a4747ab" passHref={true}>
+            {/* <Link href="https://dogged-pioneer-9796.ck.page/ad5a4747ab" passHref={true}>
                 <button className="btn btn-primary">Get Volleyball news, scores and stories in your inbox twice a week!</button>
-            </Link>
+            </Link> */}
             <script async data-uid="dc9f9547ef" src="https://dogged-pioneer-9796.ck.page/dc9f9547ef/index.js"></script>
         </div>
     )
